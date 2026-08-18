61992681aac8815d4a8bbc37e9a987debd01181d
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-18 17:16:13 +0400

    Refactor HomeScreen to enhance user interaction by integrating a new CategorySection component, and update CategoryCard to support selection state and onPress functionality.

---
+++ types/restaurant.ts
@@ -0,0 +1,12 @@
+export type Restaurant = {
+  id: number;
+  name: string;
+  slug: string;
+  categorySlug: string;
+
+  rating: number;
+  deliveryTime: string;
+  deliveryFee: number;
+
+  image: string;
+};
