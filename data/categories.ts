357868c603f5f977a6ca506087a633adc245e531
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-18 17:07:35 +0400

    Refactor HomeScreen to integrate HomeSearch component, enhancing search functionality and improving user experience.

---
+++ data/categories.ts
@@ -0,0 +1,52 @@
+import { Category } from "../types/category";
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    name: "Burgers",
+    emoji: "🍔",
+    slug: "burgers",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    emoji: "🍕",
+    slug: "pizza",
+  },
+  {
+    id: 3,
+    name: "Sushi",
+    emoji: "🍣",
+    slug: "sushi",
+  },
+  {
+    id: 4,
+    name: "Healthy",
+    emoji: "🥗",
+    slug: "healthy",
+  },
+  {
+    id: 5,
+    name: "Chicken",
+    emoji: "🍗",
+    slug: "chicken",
+  },
+  {
+    id: 6,
+    name: "Indian",
+    emoji: "🍛",
+    slug: "indian",
+  },
+  {
+    id: 7,
+    name: "Arabic",
+    emoji: "🥙",
+    slug: "arabic",
+  },
+  {
+    id: 8,
+    name: "Desserts",
+    emoji: "🍰",
+    slug: "desserts",
+  },
+];
---
+++ types/category.ts
@@ -0,0 +1,6 @@
+export type Category = {
+  id: number;
+  name: string;
+  emoji: string;
+  slug: string;
+};
