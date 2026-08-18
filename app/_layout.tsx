9c79d5f3729d616732f7617f92c31c1e828bb751
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-19 02:44:51 +0400

    Update app configuration and splash screen settings: rename app to "BiteFlow", change splash icon, modify background color, and add splash screen options. Update package dependencies to include expo-splash-screen and adjust build scripts for Android and iOS.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,14 @@ import "../global.css";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-export default function rootLayout() {
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+export default function RootLayout() {
   return (
     <>
       <StatusBar style="dark" />
