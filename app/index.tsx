59ffe9feb28b884b8349f45da7eb3cee06f28b16
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-18 17:05:41 +0400

    Update package dependencies in package.json and package-lock.json, adding @expo/vector-icons and react-dom, and adjusting react version for compatibility. Enhance HomeScreen by integrating HomeSearch component for improved functionality.

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 import { Screen } from "../components/ui/Screen";
 import { HomeHeader } from "../components/home/HomeHeader";
+import { HomeSearch } from "../components/home/HomeSearch";
 
 export default function HomeScreen() {
   return (
     <Screen>
       <HomeHeader />
+      <HomeSearch />
     </Screen>
   );
 }
