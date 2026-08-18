187ee3e3a2b4393535243f4621e8a57dff2617de
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-18 17:21:52 +0400

    Wrap HomeScreen content in a ScrollView to improve layout and scrolling behavior for better user experience.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Screen } from "../components/ui/Screen";
 import { HomeHeader } from "../components/home/HomeHeader";
 import { HomeSearch } from "../components/home/HomeSearch";
@@ -8,10 +8,15 @@ import { RestaurantSection } from "../components/home/RestaurantSection";
 export default function HomeScreen() {
   return (
     <Screen>
-      <HomeHeader />
-      <HomeSearch />
-      <CategorySection />
-      <RestaurantSection />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-8"
+      >
+        <HomeHeader />
+        <HomeSearch />
+        <CategorySection />
+        <RestaurantSection />
+      </ScrollView>
     </Screen>
   );
 }
